72bde96872654c5adafba3d0e726a77041b7a7f0
Payal Bhanudas Chavan <payal.chavan2021@gmail.com>
2026-01-16 07:32:15 +0530

    Book class created

---
+++ Object creation/Book.java
@@ -0,0 +1,27 @@
+
+public class Book {
+//variables
+	String title;
+	String author;
+	double price;
+	String color;
+	String types;
+	int publishYr;
+
+	// methods
+	void checkAvailablity() {
+		System.out.println("book is available");
+	}
+
+	void readBook() {
+		System.out.println("Reading the book");
+	}
+
+	void openBook() {
+		System.out.println("Book is open");
+	}
+
+	void closeBook() {
+		System.out.println("Book is close");
+	}
+}
