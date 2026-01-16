fd6d3fea9e8b0296b68ab573d68de803bc6c30a8
Payal Bhanudas Chavan <payal.chavan2021@gmail.com>
2026-01-16 07:39:12 +0530

    Add files via upload

---
+++ Object creation/MainClass2.java
@@ -0,0 +1,20 @@
+
+public class MainClass2 {
+	public static void main(String args[]) {
+		Car bmd = new Car();
+
+		bmd.startCar();
+		bmd.carRunning();
+		bmd.carStaring();
+		bmd.carwheels();
+		bmd.stopCar();
+		System.out.println();
+
+		Car tata = new Car();
+		tata.startCar();
+		tata.carRunning();
+		tata.carStaring();
+		tata.carwheels();
+		tata.stopCar();
+	}
+}
