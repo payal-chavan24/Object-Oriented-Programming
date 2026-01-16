3b7fdb39a1cd9b387dbc24a364d548351a967e92
Payal Bhanudas Chavan <payal.chavan2021@gmail.com>
2026-01-16 07:38:23 +0530

    Add files via upload

---
+++ Object creation/MainClass.java
@@ -0,0 +1,14 @@
+
+public class MainClass {
+	public static void main(String args[]) {
+		Laptop l = new Laptop();
+		l.brand = "hp";
+		l.modelName = "Pavillion 15";
+		l.price = 38000;
+		l.ram = 19;
+
+		l.showDetails();
+		l.performance();
+
+	}
+}
