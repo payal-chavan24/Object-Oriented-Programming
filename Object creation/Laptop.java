8775a6b5628274e02bfcae3731b8ffe78fbb2e9f
Payal Bhanudas Chavan <payal.chavan2021@gmail.com>
2026-01-16 07:36:43 +0530

    Add files via upload

---
+++ Object creation/Laptop.java
@@ -0,0 +1,18 @@
+public class Laptop{
+	String brand;
+	String modelName;
+	int ram;
+	double price;
+	
+	void performance() {
+		System.out.println("Highly performance Laptop");
+	}
+	void showDetails() {
+		System.out.println("Brand ="+brand);
+		System.out.println("Model Name ="+modelName);
+		System.out.println("Ram ="+ram);
+		System.out.println("Price ="+price);
+		
+	}
+	
+}
