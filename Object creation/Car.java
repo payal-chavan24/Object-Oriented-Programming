a263794a9f116287fceb54d3ed3bc4d6c46e75b1
Payal Bhanudas Chavan <payal.chavan2021@gmail.com>
2026-01-16 07:37:41 +0530

    Add files via upload

---
+++ Object creation/Car.java
@@ -0,0 +1,28 @@
+
+public class Car {
+//variable
+	int modelYr;
+	String brand;
+	String color;
+	double price;
+
+	void carRunning() {
+		System.out.println("car is running");
+	}
+
+	void carStaring() {
+		System.out.println("staring Activity");
+	}
+
+	void carwheels() {
+		System.out.println("wheels are moving");
+	}
+
+	void startCar() {
+		System.out.println("car is started");
+	}
+
+	void stopCar() {
+		System.out.println("car is stoped");
+	}
+}
