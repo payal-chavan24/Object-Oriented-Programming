e2f08cfd0c49d97a4cea8f49c5453c805eece994
Payal Bhanudas Chavan <payal.chavan2021@gmail.com>
2026-01-16 07:33:13 +0530

    Add files via upload

---
+++ Object creation/MainClass3.java
@@ -0,0 +1,19 @@
+
+public class MainClass3 {
+	public static void main(String[] args) {
+		// creating object
+		Book b1 = new Book();
+		b1.openBook();
+		b1.checkAvailablity();
+		b1.readBook();
+		b1.closeBook();
+		System.out.println();
+		Book b2 = new Book();
+		b2.openBook();
+		b2.checkAvailablity();
+		b2.readBook();
+		b2.closeBook();
+
+	}
+
+}
