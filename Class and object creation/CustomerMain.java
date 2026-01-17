ca661da458ac5ac75dab48e92984bb011133f79e
Payal Bhanudas Chavan <payal.chavan2021@gmail.com>
2026-01-17 22:10:01 +0530

    Add files via upload

---
+++ Class and object creation/CustomerMain.java
@@ -0,0 +1,14 @@
+
+public class CustomerMain {
+
+	public static void main(String[] args) {
+		Customer c1 =new Customer();
+		c1.customerName="Rahul sharma";
+		c1.customerId=34;
+		c1.phoneNumber=6789345678L;
+		System.out.println("Name of the Customer"+c1.customerName);
+		System.out.println("Customer Id ="+c1.customerId);
+		System.out.println("Phone number="+c1.phoneNumber);
+	}
+
+}
