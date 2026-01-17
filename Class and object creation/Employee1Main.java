355b057f13038e656449b3d8796b5b0b54c6cbf0
Payal Bhanudas Chavan <payal.chavan2021@gmail.com>
2026-01-17 22:00:31 +0530

    Add files via upload

---
+++ Class and object creation/Employee1Main.java
@@ -0,0 +1,21 @@
+public class Employee1Main {
+	public static void main(String args[]) {
+		Employee1 emp1 = new Employee1();
+		String a = emp1.empName = "Ramesh";
+		double b = emp1.salary = 25000;
+		int c = emp1.empId = 456;
+		System.out.println("Name of Employee =" + a);
+		System.out.println("Salary" + b + "/-");
+		System.out.println("Emplyoee Id=" + c);
+		System.out.println("------");
+		Employee1 emp2 = new Employee1();
+		String d = emp2.empName = "Rahul Sharma";
+		double e = emp2.salary = 56000;
+		int f = emp2.empId = 567;
+		System.out.println("Name of Employee =" + d);
+		System.out.println("Salary" + e + "/-");
+		System.out.println("Emplyoee Id=" + f);
+		System.out.println("------");
+
+	}
+}
