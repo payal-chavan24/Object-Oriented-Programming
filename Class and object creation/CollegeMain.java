3ed09db04d28c9f66a3a25b611f4c28cfe6915f5
Payal Bhanudas Chavan <payal.chavan2021@gmail.com>
2026-01-17 22:08:40 +0530

    Add files via upload

---
+++ Class and object creation/CollegeMain.java
@@ -0,0 +1,16 @@
+
+public class CollegeMain {
+
+	public static void main(String[] args) {
+		College c1=new College();
+		c1.collegeName=" Autonomous degree college";
+		c1.city="kalbhurgi"	;	
+		c1.rating=4.5f;
+		System.out.println("College Name ="+c1.collegeName);
+		System.out.println("City ="+c1.collegeName);
+		System.out.println("Rating ="+c1.rating);
+				
+
+	}
+
+}
