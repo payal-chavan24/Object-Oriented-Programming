20e306b3fcb455702403a2ea2436178988f45a1f
Payal Bhanudas Chavan <payal.chavan2021@gmail.com>
2026-01-17 22:06:04 +0530

    Add files via upload

---
+++ Class and object creation/BikeMain.java
@@ -0,0 +1,14 @@
+
+public class BikeMain {
+
+	public static void main(String[] args) {
+		Bike b1=new Bike();
+		b1.bikeName="Hero Honda";
+		b1.enginecc=145;
+		b1.price=2.68;
+		System.out.println("Name of bike ="+b1.bikeName);
+		System.out.println("Enginecc ="+b1.enginecc);
+		System.out.println("Price ="+b1.price+"lakh");
+	}
+
+}
