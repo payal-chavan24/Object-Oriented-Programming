879ec5616aa6f30679b3451419e2a5551da89f5a
Payal Bhanudas Chavan <payal.chavan2021@gmail.com>
2026-01-17 22:16:00 +0530

    Add files via upload

---
+++ Class and object creation/StudentMain.java
@@ -0,0 +1,15 @@
+
+public class StudentMain {
+
+	public static void main(String[] args) {
+		Student1 s1 = new Student1();
+		String a = s1.name = "Payal Chavan";
+		int b = s1.rollNo = 24;
+		int c = s1.marks = 87;
+		System.out.println("Name=" + a);
+		System.out.println("Roll no =" + b);
+		System.out.println("Marks =" + c);
+
+	}
+
+}
