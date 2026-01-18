cf390309c1da5f5dadb0e69c644c82aa5785fcbf
Payal Bhanudas Chavan <payal.chavan2021@gmail.com>
2026-01-18 12:26:00 +0530

    Add files via upload

---
+++ Create and object creation 2/Employee.java
@@ -0,0 +1,36 @@
+
+public class Employee {
+	// global variable
+	int empId;
+	String empName;
+	double salary;
+	char gender;
+	int age;
+
+	// method
+	void greet() {
+		System.out.println("Hello welcome to company");
+	}
+
+	void printDetails() {
+		System.out.println("Emplyoee Id =" + empId);
+		System.out.println("Emplyoee age =" + age);
+		System.out.println("Emplyoee Name =" + empName);
+		System.out.println("Emplyoee Salary  =" + salary);
+		System.out.println("Emplyoee gender  =" + gender);
+	}
+
+	void experience() {
+		System.out.println("Fresher");
+	}
+
+	void education() {
+		System.out.println("Under Graguate");
+
+	}
+
+	void unity() {
+		System.out.println("Emplyoee are working in unity");
+	}
+
+}
