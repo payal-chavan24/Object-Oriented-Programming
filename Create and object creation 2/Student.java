12d9e77588f477e632d19118a7ff9ffebdbcdcd7
Payal Bhanudas Chavan <payal.chavan2021@gmail.com>
2026-01-18 12:35:56 +0530

    Add files via upload

---
+++ Create and object creation 2/Student.java
@@ -0,0 +1,38 @@
+
+public class Student {
+	// variable
+	String name;
+	int age;
+	int id;
+	String city;
+	String gender;
+	char section;
+
+//method
+	void studying() {
+		System.out.println(" Student are studying ");
+	}
+
+	void reading() {
+		System.out.println("Student are reading");
+	}
+
+	void wrinting() {
+		System.out.println("Student are writing");
+	}
+
+	void display() {
+		System.out.println("Student name =" + name);
+		System.out.println("Student age =" + age);
+		System.out.println("Student id =" + id);
+		System.out.println("City =" + city);
+		System.out.println("Gender =" + gender);
+		System.out.println("Section =" + section);
+	}
+
+	void rule() {
+		System.out.println("Student are following Rules");
+
+	}
+
+}
