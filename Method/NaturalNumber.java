7b82e2712f061700419227e41517171b7aa6c530
Payal Bhanudas Chavan <payal.chavan2021@gmail.com>
2026-01-20 23:48:08 +0530

    Add files via upload

---
+++ Method/NaturalNumber.java
@@ -0,0 +1,10 @@
+// Write a method to print the first 10 natural numbers.
+package com.number;
+
+public class NaturalNumber {
+	public void displaynumber() {
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(i);
+		}
+	}
+}
