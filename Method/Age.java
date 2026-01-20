1d890cd3b49c9fffcec87127d196a3441de0d074
Payal Bhanudas Chavan <payal.chavan2021@gmail.com>
2026-01-20 23:27:35 +0530

    Add files via upload

---
+++ Method/Age.java
@@ -0,0 +1,10 @@
+//Write a method that returns your age (return type, no parameters).
+package com.number;
+
+public class Age {
+	int age;
+
+	public void displayAge() {
+		System.out.println("Age =" + age);
+	}
+}
