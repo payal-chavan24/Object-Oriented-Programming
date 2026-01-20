7ef6caaad32d1af5518e56de5055e5430aac6270
Payal Bhanudas Chavan <payal.chavan2021@gmail.com>
2026-01-20 23:28:58 +0530

    Add files via upload

---
+++ Method/SumMain.java
@@ -0,0 +1,8 @@
+package com.number;
+
+public class SumMain {
+	public static void main(String args[]) {
+		Sum add = new Sum();
+		add.addition(4, 5);// here we have intalized a value for a and b variable
+	}
+}
