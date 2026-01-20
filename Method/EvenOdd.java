e6928765b92972a9d6c0fa504866e34f2b936e5c
Payal Bhanudas Chavan <payal.chavan2021@gmail.com>
2026-01-20 23:58:02 +0530

    Add files via upload

---
+++ Method/EvenOdd.java
@@ -0,0 +1,13 @@
+//Write a method to check whether a number is even or odd.
+package com.number;
+
+public class EvenOdd {
+	public void checknumber() {
+		int num1 = 10;
+		if (num1 % 2 == 0) {
+			System.out.println(num1 + " is " + "Even number");
+		} else {
+			System.out.println(num1 + " is " + "Odd number");
+		}
+	}
+}
