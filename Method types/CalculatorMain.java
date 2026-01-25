f87508461d7fb6ecb120421e5070302e9d81f06c
Payal Bhanudas Chavan <payal.chavan2021@gmail.com>
2026-01-25 23:54:37 +0530

    Add files via upload

---
+++ Method types/CalculatorMain.java
@@ -0,0 +1,13 @@
+
+public class CalculatorMain {
+
+	public static void main(String[] args) {
+		Calculator c = new Calculator();
+		System.out.println("Addition =" + c.printAdd(56, 98));
+		System.out.println("substraction =" + c.printsub(67, 98));
+		System.out.println("Multiply =" + c.printmultiply(87, 6));
+		System.out.println("Division =" + c.printdiv(65, 5));
+		System.out.println("Modulue =" + c.printmodule(67, 5));
+	}
+
+}
