4a11f5f550ea380935c5315475ef2e143e8bb463
Payal Bhanudas Chavan <payal.chavan2021@gmail.com>
2026-01-26 00:04:07 +0530

    Main class file

--- Method types/CalculatorMain.java
+++ Method types/CalculatorMain.java
@@ -5,9 +5,10 @@ public class CalculatorMain {
 		Calculator c = new Calculator();
 		System.out.println("Addition =" + c.printAdd(56, 98));
 		System.out.println("substraction =" + c.printsub(67, 98));
-		System.out.println("Multiply =" + c.printmultiply(87, 6));
+		System.out.println("Multiplication =" + c.printmultiply(87, 6));
 		System.out.println("Division =" + c.printdiv(65, 5));
-		System.out.println("Modulue =" + c.printmodule(67, 5));
+		System.out.println("Modulus =" + c.printmodule(67, 5));
 	}
 
 }
+
