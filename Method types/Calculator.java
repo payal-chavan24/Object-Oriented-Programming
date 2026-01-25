41feef6bfb75ffafcdf1f5ee33ec3c912ab99f08
Payal Bhanudas Chavan <payal.chavan2021@gmail.com>
2026-01-25 23:53:46 +0530

    Create Calculator.java

---
+++ Method types/Calculator.java
@@ -0,0 +1,23 @@
+//parameter with return type.
+public class Calculator {
+	public int printAdd(int a, int b) {
+		// System.out.println("Addition");
+		return a + b;
+	}
+
+	public int printsub(int a, int b) {
+		return a - b;
+	}
+
+	public int printmultiply(int a, int b) {
+		return a * b;
+	}
+
+	public int printdiv(int a, int b) {
+		return a / b;
+	}
+
+	public int printmodule(int a, int b) {
+		return a % b;
+	}
+}
