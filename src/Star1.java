06d93da8f958fa83202f59d3cdd803449aad5d8f
Payal Bhanudas Chavan <payal.chavan2021@gmail.com>
2026-02-08 23:45:21 +0530

    Add files via upload

---
+++ src/Star1.java
@@ -0,0 +1,14 @@
+//star
+public class Star1 {
+
+	public static void main(String[] args) {
+		for(int i=3;i>=1;i--) {
+			for(int j=1;j<=i;j++) {
+				System.out.print("*");
+			}
+			System.out.println(" ");
+		}
+
+	}
+
+}
---
+++ src/Star4.java
@@ -0,0 +1,32 @@
+//squre
+public class Star4 {
+
+	public static void main(String[] args) {
+//	for(int i=1;i<=3;i++) {
+//		for(int j=1;j<=3;j++) {
+//			System.out.print("*");
+//		}
+//		System.out.println(" ");
+//	}
+//		for( int i=1;i<=4;i++) {
+//			for(int j=1;j<=i;j++) {
+//				System.out.print("*");
+//			}
+//			System.out.println("");
+//		}
+//		for(int i=4;i>=1;i--) {
+//			for(int j=1;j<=i;j++){
+//				System.out.print("*");
+//			}
+//			System.out.println(" ");
+//		}
+		for(int i=1;i<=5;i++){
+			for(int j=1;j<=i;j++) {
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+
+	}
+
+}
