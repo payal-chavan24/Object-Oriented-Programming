fe8db9aafacce3ce526e17d1666c7425e9034a5d
Payal Bhanudas Chavan <payal.chavan2021@gmail.com>
2026-03-02 22:10:08 +0530

    Add files via upload

---
+++ PracticeQuestion.java
@@ -0,0 +1,11 @@
+package com.string;
+
+public class PracticeQuestion {
+public static void main(String[] args) {
+	String s="payal";
+	String s1=new String ("chavan");
+	String s2=s.concat("chavan");
+	System.out.println(s==s1);
+	System.out.println(s1==s2);
+}
+}
