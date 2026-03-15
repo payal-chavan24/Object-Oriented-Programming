1b2493fafc9a66f32281a98fafda64b2cd1220de
Payal Bhanudas Chavan <payal.chavan2021@gmail.com>
2026-03-15 15:00:28 +0530

    Add files via upload

---
+++ SumOfAllElements.java
@@ -0,0 +1,16 @@
+package com.twodimensionarray;
+
+public class SumOfAllElements{
+public static void main(String[] args) {
+	int [][] arr= {{1,2,3},{4,5,6}};
+	int sum=0;
+	for(int i=0;i<arr.length;i++) {
+		for(int j=0;j<arr[i].length;j++) {
+			sum=sum+arr[i][j];
+			
+		}
+		
+	}
+	System.out.println("Sum of elements are ="+sum);
+}
+}
