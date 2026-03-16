5ea85e85c0c4dc585b26d48cd62fc2394c0d65b5
Payal Bhanudas Chavan <payal.chavan2021@gmail.com>
2026-03-16 23:01:13 +0530

    ArrayPrractice

---
+++ SumElements.java
@@ -0,0 +1,12 @@
+//write a program to find sum of all elements in an array
+public class SumElements{
+public static void main(String args[]){
+int sum=0;
+int[] arr2={2,1,3,4,7};
+for(int i=0;i<arr2.length;i++){
+sum=sum+arr2[i];
+}
+System.outt.println("Sum ="+sum);
+}
+}
+
